4c79254bf5d4b1052e727e688f3cde2038daff13
amimaro <amir.zahlan@gmail.com>
2020-02-26 23:44:37 -0300

    deploy

---
+++ precache-manifest.598df09959175f9cdef8c921dd3bba72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf368cc47aa9a848ffb",
+    "url": "/kombucha-calculator/css/app.9ea4eb67.css"
+  },
+  {
+    "revision": "49ed55f3a72de0ca12e5",
+    "url": "/kombucha-calculator/css/chunk-vendors.b3749ab6.css"
+  },
+  {
+    "revision": "45f31cc7f574891477da539785aca5e6",
+    "url": "/kombucha-calculator/img/wine-1237340_1280.45f31cc7.jpg"
+  },
+  {
+    "revision": "e97d4f00575b63bff41ecbd9841490bc",
+    "url": "/kombucha-calculator/index.html"
+  },
+  {
+    "revision": "70b4ef533a5015d0446f",
+    "url": "/kombucha-calculator/js/about.d03acf49.js"
+  },
+  {
+    "revision": "7cf368cc47aa9a848ffb",
+    "url": "/kombucha-calculator/js/app.61e0af1b.js"
+  },
+  {
+    "revision": "49ed55f3a72de0ca12e5",
+    "url": "/kombucha-calculator/js/chunk-vendors.d0f21054.js"
+  },
+  {
+    "revision": "09f7828effae032d9162a70dc7b5f845",
+    "url": "/kombucha-calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kombucha-calculator/robots.txt"
+  }
+]);
